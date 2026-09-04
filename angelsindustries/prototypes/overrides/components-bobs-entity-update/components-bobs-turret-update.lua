7511ca1d38c40cee1e7b7b3488e7566e70d92b29
Jonas Özbay <jo.spam@gmx.net>
2026-09-04 12:05:58 +0200

    correct replacing of bob-battery-x by angelsindustries' batteries

--- angelsindustries/prototypes/overrides/components-bobs-entity-update/components-bobs-turret-update.lua
+++ angelsindustries/prototypes/overrides/components-bobs-entity-update/components-bobs-turret-update.lua
@@ -13,7 +13,7 @@ if angelsmods.industries.components then
       {
         name = "bob-laser-turret-3",
         ingredients = {
-          { type = "item", name = "battery-3", amount = mods["bobplates"] and "battery-3" or "battery-1" },
+          { type = "item", name = "battery-3", amount = mods["bobplates"] and "bob-battery-2" or "battery" },
           { type = "item", name = "angels-plate-aluminium", amount = "steel-plate" },
           { type = "item", name = "circuit-orange-loaded", amount = 20 },
         },
@@ -21,14 +21,14 @@ if angelsmods.industries.components then
       {
         name = "bob-laser-turret-4",
         ingredients = {
-          { type = "item", name = "battery-4", amount = mods["bobplates"] and "battery-3" or "battery-1" },
+          { type = "item", name = "battery-4", amount = mods["bobplates"] and "bob-battery-2" or "battery" },
           { type = "item", name = "angels-plate-titanium", amount = "steel-plate" },
         },
       },
       {
         name = "bob-laser-turret-5",
         ingredients = {
-          { type = "item", name = "battery-5", amount = mods["bobplates"] and "battery-6" or "battery-1" },
+          { type = "item", name = "battery-5", amount = mods["bobplates"] and "bob-battery-3" or "battery" },
           { type = "item", name = "angels-plate-tungsten", amount = "steel-plate" },
           { type = "item", name = "circuit-yellow-loaded", amount = "circuit-blue-loaded" },
         },
